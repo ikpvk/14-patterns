3665b0132525437911aa44449400c3652d8dd535
Krishna <krishnaprasad.vk@gmail.com>
2024-06-12 23:58:19 +0530

    Adding code for 2 pointers

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 Practicing the codes from https://hackernoon.com/14-patterns-to-ace-any-coding-interview-question-c5bb3357f6ed
 
 1. Sliding window
+
+2. Two Pointers or Iterators
---
+++ learningcode/src/main/java/com/fourteenpatterns/twopinters/Solution.java
@@ -0,0 +1,68 @@
+package com.fourteenpatterns.twopinters;
+
+/*
+Given two strings s and t, return true if they are equal
+when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        String s = "ab##", t = "c#d#";
+        System.out.println(areStringsEqual(s,t));
+    }
+
+    public static boolean areStringsEqual(String s, String t) {
+        int p1 = s.length()-1;
+        int p2 = t.length()-1;
+        if(p1==-1 && p2==-1) {
+            return true;
+        }
+        while (p1>=0 && p2>=0) {
+            System.out.println(p1+ " "+p2);
+            if (s.charAt(p1) != t.charAt(p2)) {
+                return false;
+            }
+            p1 = getNextPointer(s,p1);
+            System.out.println(p1);
+            p2 = getNextPointer(t,p2);
+            System.out.println(p2);
+        }
+        System.out.println("final :"+p1+ " "+p2);
+        if(p1==p2) {
+            return true;
+        }
+        return false;
+    }
+
+    private static int getNextPointer(String s, int p) {
+        System.out.println("String "+s+" p "+p);
+        if(p<0) {
+            return p;
+        }
+        int bckSpcCount = 0;
+        if(s.charAt(p-1)!='#') {
+            return p-1;
+        }
+        bckSpcCount++;
+        while(bckSpcCount>0) {
+            p--;
+            if(p<0) {
+                return -1;
+            }
+            if(s.charAt(p)=='#') {
+                bckSpcCount++;
+            }
+        }
+        return -1;
+    }
+}
