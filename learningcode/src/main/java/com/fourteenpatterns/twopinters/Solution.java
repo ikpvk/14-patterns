ba07486ad29fc85a6d41d95debfa03fdea40d294
Krishna Prasad <krishnaprasad.vk@gmail.com>
2025-03-31 23:47:17 +0530

    next iteration for 2 pointer

--- learningcode/src/main/java/com/fourteenpatterns/twopinters/Solution.java
+++ learningcode/src/main/java/com/fourteenpatterns/twopinters/Solution.java
@@ -1,6 +1,8 @@
 package com.fourteenpatterns.twopinters;
 
 /*
+Comparing strings that contain backspaces (medium)
+
 Given two strings s and t, return true if they are equal
 when both are typed into empty text editors. '#' means a backspace character.
 
@@ -17,59 +19,56 @@ Explanation: Both s and t become "".
 public class Solution {
 
     public static void main(String[] args) {
-        String s = "ab##", t = "c#d#";
-        System.out.println(areStringsEqual(s,t));
+        String s = "nzp#o#g", t = "b#nzp#o#g";
+        System.out.println(areStringsEqual(s, t));
+//        System.out.println(getNextPointer("asbc##",1));
     }
 
     public static boolean areStringsEqual(String s, String t) {
-        int p1 = s.length()-1;
-        int p2 = t.length()-1;
-        if(p1==-1 && p2==-1) {
+        int p1 = s.length() - 1;
+        int p2 = t.length() - 1;
+        if (p1 == -1 && p2 == -1) {
             return true;
         }
-        while (p1>=0 && p2>=0) {
-            System.out.println("Iteration "+p1+ " "+p2);
-            if (s.charAt(p1) != t.charAt(p2)) {
+        while (p1 >= 0 && p2 >= 0) {
+            int nextp2 = getNextPointer(t, p2);
+            int nextp1 = getNextPointer(s, p1);
+            if (nextp2 < 0 && nextp1 < 0) {
+                return true;
+            }
+            if ((nextp2 < 0 && nextp1 >= 0) || (nextp1 < 0 && nextp2 >= 0)) {
                 return false;
             }
-            p1 = getNextPointer(s,p1);
-            System.out.println("p1 :"+p1);
-            p2 = getNextPointer(t,p2);
-            System.out.println("p2 :"+p2);
+            if (s.charAt(nextp1) == t.charAt(nextp2)) {
+                p1 = nextp1 - 1;
+                p2 = nextp2 - 1;
+            } else {
+                return false;
+            }
         }
-        System.out.println("final :"+p1+ " "+p2);
-        if(p1==p2) {
+        if (p1 == p2) {
             return true;
         }
         return false;
     }
 
     private static int getNextPointer(String s, int p) {
-        System.out.println("String "+s+" p "+p);
-        if(p<=0) {
-            return -1;
+        if (p <= 0) {
+            return p;
         }
-        int bckSpcCount = 0;
-        if(s.charAt(p)=='#') {
-            bckSpcCount++;
-            while(bckSpcCount>0) {
-                System.out.println(bckSpcCount+" ; "+p);
-                p--;
-                if(p<0) {
-                    return 0;
-                }
-                if(s.charAt(p)=='#') {
-                    bckSpcCount++;
-                }
-                bckSpcCount--;
-            }
-        } else {
-            if(s.charAt(p-1)=='#') {
-                return getNextPointer(s,p-1);
+        int backSpaceCounter = 0;
+        int iterator = p;
+        while (iterator >= 0) {
+            if (s.charAt(iterator) == '#') {
+                backSpaceCounter++;
+                iterator--;
+            } else if (backSpaceCounter > 0) {
+                backSpaceCounter--;
+                iterator--;
             } else {
-                return p - 1;
+                break;
             }
         }
-        return p-1;
+        return iterator;
     }
 }
